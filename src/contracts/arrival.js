62b4434dacc954e989708290c3343a9ac551d363
findolor <anakisci@gmail.com>
2022-11-26 11:13:25 +0300

    Add check membership helper method

--- src/contracts/arrival.js
+++ src/contracts/arrival.js
@@ -1,19 +1,17 @@
+import { checkMembership } from "./checkMembership"
+
 export const queryDayOneArrival = async (client, address) => {
-  return client.queryContractSmart(
-    // Day One Arrival CW4 Group Contract
+  return checkMembership(
+    client,
     "juno1j2ysyynjfmmxcj7pmctqzmyjvf3flyckf7hexllvjgx5h8k5j7cs26cj4c",
-    {
-      member: { addr: address },
-    }
+    address
   )
 }
 
 export const queryDayTwoArrival = async (client, address) => {
-  return client.queryContractSmart(
-    // Day Two Arrival CW4 Group Contract
+  return checkMembership(
+    client,
     "juno1t3pu7c4r8476gxuc5037c4y94x9sxspsxg7gync07hnmjset7rvsln3qzf",
-    {
-      member: { addr: address },
-    }
+    address
   )
 }
--- src/contracts/guestType.js
+++ src/contracts/guestType.js
@@ -1,26 +1,26 @@
+import { checkMembership } from "./checkMembership"
+
 export const queryGuestType = async (client, address) => {
-    return client.queryContractSmart(
-      // Guest Type CW4 Group Contract
-      "juno1kwmpjp6we33gvkcnfka33l6l55ckyyvryyhnlpue3g7luh3ftd6qz3wqw5",
-      {
-        member: { addr: address },
-      }
-    )
+  return checkMembership(
+    client,
+    "juno1kwmpjp6we33gvkcnfka33l6l55ckyyvryyhnlpue3g7luh3ftd6qz3wqw5",
+    address
+  )
+}
+
+export const getGuestType = (weight) => {
+  switch (weight) {
+    case 1:
+      return "Brunch Guest"
+    case 2:
+      return "Dinner Guest"
+    case 3:
+      return "Maker Guest"
+    case 4:
+      return "Maker"
+    case 5:
+      return "Special Guest"
+    default:
+      return "Unknown Guest"
   }
-  
-  export const getGuestType = (weight) => {
-    switch (weight) {
-      case 1:
-        return "Brunch Guest"
-      case 2:
-        return "Dinner Guest"
-      case 3:
-        return "Maker Guest"
-      case 4:
-        return "Maker"
-      case 5:
-        return "Special Guest"
-      default:
-        return "Unknown Guest"
-    }
-  }
+}
