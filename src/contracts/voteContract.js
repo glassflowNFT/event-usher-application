2dd94bacff84334a85a95ab7f413a2b45438941d
findolor <anakisci@gmail.com>
2022-12-02 13:14:11 +0300

    Add helper methods for the voting contract

--- src/contracts/voteContract.js
+++ src/contracts/voteContract.js
@@ -1,5 +1,7 @@
 import { checkMembership } from "./checkMembership"
 
+const VOTING_CONTRACT = "juno..."
+
 export const queryJudge = async (client, address ) => {
     return checkMembership(
             client,
@@ -16,4 +18,74 @@ export const getJudge = (weight) => {
         default:
           return "non-judge"
     }
+}
+
+export const queryEntries = (client, category, startAfter = undefined, limit = undefined) => {
+  return client.queryContractSmart(
+    VOTING_CONTRACT,
+    {
+      entries: {
+        category,
+        start_after: startAfter,
+        limit,
+      }
+    }
+  )
+}
+
+export const queryEntry = (client, category, entryId) => {
+  return client.queryContractSmart(
+    VOTING_CONTRACT,
+    {
+      entry: {
+        category,
+        entry_id: entryId
+      }
+    }
+  )
+}
+
+export const queryVotes = (client, entryId, makerAddress) => {
+  return client.queryContractSmart(
+    VOTING_CONTRACT,
+    {
+      votes: {
+        entry_id: entryId,
+        maker_addr: makerAddress
+      }
+    }
+  )
+}
+
+export const queryTallyVotes = (client, entryId, startAfter, limit) => {
+  return client.queryContractSmart(
+    VOTING_CONTRACT,
+    {
+      tally_votes: {
+        entry_id: entryId,
+        start_after: startAfter,
+        limit
+      }
+    }
+  )
+}
+
+export const vote = (client, sender, category, entryId, { look, smell, taste, postMelt }) => {
+  return client.execute(
+    sender,
+    VOTING_CONTRACT,
+    {
+      vote: {
+        category,
+        entry_id: entryId,
+        votes: {
+          look,
+          smell,
+          taste,
+          post_melt: postMelt
+        }
+      }
+    },
+    'auto'
+  )
 }
